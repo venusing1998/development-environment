b1d6f4b6f4e088cf192e8699d8e0ead00c069380
Chris <christoleavenow@gmail.com>
2018-07-12 10:18:59 +0800

    update

--- CentOS 7/get-shadowsocks.sh
+++ CentOS 7/get-shadowsocks.sh
@@ -1,5 +1,5 @@
 yum -y install wget
 cd ~/Downloads
-wget -P --no-check-certificate 
+wget -P --no-check-certificate https://github.com/venusing1998/development-environment/blob/master/CentOS%207/shadowsocks.sh
 chmod +x ss.sh
 ./ss.sh 2>&1 | tee shadowsocks.log
