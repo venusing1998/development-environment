84255d87130cbe2c11ed7ebb0d291d42f8df9a6e
Chris <christoleavenow@gmail.com>
2018-07-18 23:50:23 +0800

    modify get-nginx.sh

--- CentOS 7/get-nginx.sh
+++ CentOS 7/get-nginx.sh
@@ -1,7 +1,8 @@
-touch /etc/yum.repos.d/nginx.repo
-echo '[nginx]' >> /etc/yum.repos.d/nginx.repo
-echo 'name=nginx repo' >> /etc/yum.repos.d/nginx.repo
-echo 'baseurl=http://nginx.org/packages/centos/7/$basearch/' >> /etc/yum.repos.d/nginx.repo
-echo 'gpgcheck=0' >> /etc/yum.repos.d/nginx.repo
-echo 'enabled=1' >> /etc/yum.repos.d/nginx.repo
-yum -y install nginx
+cd ~/Downloads
+wget https://nginx.org/download/nginx-1.14.0.tar.gz
+tar -xzvf nginx-1.14.0.tar.gz
+cd nginx-1.14.0
+mkdir -p /usr/local/nginx
+./configure --user=nobody --group=nobody --prefix=/usr/local/nginx --with-http_stub_status_module --with-cc-opt='-O3' --with-cpu-opt=opteron
+make && make install
+ln ‐sv /usr/local/nginx/sbin/nginx /usr/sbin/nginx
