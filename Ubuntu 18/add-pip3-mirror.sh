3dd61e013d059f1c53012ffa5681705b10fdd174
Chris <christoleavenow@gmail.com>
2018-07-30 21:47:33 +0800

    add add-ubuntu18-mirror.sh sources.list

--- Ubuntu 18/add-pip3-mirror.sh
+++ Ubuntu 18/add-pip3-mirror.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/.config/pip
-wget -P ~/.config/pip https://raw.githubusercontent.com/venusing1998/development-environment/master/Ubuntu%2018/pip.conf
+cd ~/Downloads
+wget https://raw.githubusercontent.com/venusing1998/development-environment/master/Ubuntu%2018/pip.conf
