4061cb30d6bfe615392c46734c6a02919afada07
Chris <christoleavenow@gmail.com>
2018-07-31 20:01:44 +0800

    confirm

--- Ubuntu 18/add-ubuntu18-mirror.sh
+++ Ubuntu 18/add-ubuntu18-mirror.sh
@@ -1,5 +1,5 @@
 cd ~/Downloads
 wget https://raw.githubusercontent.com/venusing1998/development-environment/master/Ubuntu%2018/sources.list
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo mv ~/Downloads/sources.list /etc/apt/sources.list
+sudo cp ~/Downloads/sources.list /etc/apt/sources.list
 sudo apt update
--- Ubuntu 18/get-zsh.sh
+++ Ubuntu 18/get-zsh.sh
@@ -3,4 +3,6 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 cd ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+mv ~/.zshrc ~/.zshrc.bak
 wget -P ~ https://raw.githubusercontent.com/venusing1998/development-environment/master/Ubuntu%2018/.zshrc
+source ~/.zshrc
