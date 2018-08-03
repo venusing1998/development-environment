f0e5becc9beb035b0b3d4caaf41d945d07e0fb51
Chris <christoleavenow@gmail.com>
2018-08-04 01:51:44 +0800

    add .gitconfig get-git.sh

---
+++ Ubuntu 18/get-git.sh
@@ -0,0 +1,3 @@
+sudo apt -y install git
+wget -P ~/Downloads https://raw.githubusercontent.com/venusing1998/development-environment/master/Ubuntu%2018/.gitconfig
+cp ~/Downloads/.gitconfig ~/.gitconfig
