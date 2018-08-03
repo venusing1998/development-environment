4d2246d398dbca0997cf24bd319824760700e362
Chris <christoleavenow@gmail.com>
2018-08-04 03:16:31 +0800

    update

---
+++ CentOS 7/install-git.sh
@@ -0,0 +1,3 @@
+sudo yum -y install git
+wget -P ~/Downloads https://raw.githubusercontent.com/venusing1998/development-environment/master/CentOS%207/.gitconfig
+cp ~/Downloads/.gitconfig ~/.gitconfig
